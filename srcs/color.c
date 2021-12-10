9edcf74c9acec393d5d84251959cdd3a1aa4393c
Logan SIDAN <logan.sidan@icloud.com>
2021-12-10 21:33:48 +0100

    Improve formula

--- srcs/color.c
+++ srcs/color.c
@@ -6,41 +6,41 @@
 /*   By: lsidan <lsidan@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 18:50:28 by lsidan            #+#    #+#             */
-/*   Updated: 2021/12/10 16:38:37 by lsidan           ###   ########.fr       */
+/*   Updated: 2021/12/10 21:29:34 by lsidan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fractol.h"
 
-int	lerp(int v0, int v1, double p)
+int	lerp(int v0, int v1, double t)
 {
-	return ((int)((double)v0 + (v1 - v0) * p));
+	return ((int)((double)v0 + t * (v1 - v0)));
 }
 
-t_color	color_lerp(t_color col1, t_color col2, double p)
+t_color	color_lerp(t_color col1, t_color col2, double t)
 {
 	t_color	col;
 
 	if (col1.value == col2.value)
 		return (col1);
-	col.rgba.r = (t_char)lerp((int)col1.rgba.r, (int)col2.rgba.r, p);
-	col.rgba.g = (t_char)lerp((int)col1.rgba.g, (int)col2.rgba.g, p);
-	col.rgba.b = (t_char)lerp((int)col1.rgba.b, (int)col2.rgba.b, p);
+	col.rgba.r = (t_char)lerp((int)col1.rgba.r, (int)col2.rgba.r, t);
+	col.rgba.g = (t_char)lerp((int)col1.rgba.g, (int)col2.rgba.g, t);
+	col.rgba.b = (t_char)lerp((int)col1.rgba.b, (int)col2.rgba.b, t);
 	col.rgba.a = (t_char)0x00;
 	return (col);
 }
 
 t_color	linear_color(double i, int max, t_pal *pal)
 {
-	double		index;
+	double		_i;
 	double		p;
 	int			c;
 
-	index = i / max;
+	_i = i / max;
 	c = pal->count - 1;
-	p = fmod(index, 1.0f / c) * c;
-	return (color_lerp((t_color)(pal->colors[(int)(index * c) + 1]), \
-						(t_color)(pal->colors[(int)(index * c)]), (1 - p)));
+	p = fmod(_i, 1.0f / c) * c;
+	return (color_lerp((t_color)(pal->colors[(int)(_i * c) + 1]), \
+						(t_color)(pal->colors[(int)(_i * c)]), (1 - p)));
 }
 
 t_color	smooth(t_data *d, int iter)
